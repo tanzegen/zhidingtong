68d8fc428885515a5569cac9af37dce1e17fd68d
bzttzg <1295620364@qq.com>
2021-03-31 17:34:55 +0800

    update 注入问题

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -28,11 +28,13 @@ class ConfigProvider
                 ],
             ],
             'publish' => [
-                'id' => 'config',
-                'description' => 'main config by this package', // 描述
-                // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
-                'source' => __DIR__ . '/../publish/zhidingtong.php',  // 对应的配置文件路径
-                'destination' => BASE_PATH . '/config/autoload/zhidingtong.php', // 复制为这个路径下的该文件
+                [
+                    'id' => 'config',
+                    'description' => 'main config by this package', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/../publish/zhidingtong.php',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/config/autoload/zhidingtong.php', // 复制为这个路径下的该文件
+                ]
             ]
         ];
     }
